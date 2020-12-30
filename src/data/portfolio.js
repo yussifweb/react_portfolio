095c9e651e637ab84e9d4c621c7c9171fe7e1729
Yussif <djiceman.gh@gmail.com>
2020-12-30 22:39:23 +0000

    uploaded images

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,12 +1,23 @@
+// export const images = [
+//     {src: "images/ediot.jpg", alt: "DJ Koded Ediot Refix"},
+//     {src: "images/lims.jpg", alt: "Lims Radio"},
+//     {src: "images/byfball.jpg", alt: "Bekwai Youth Football Academy"},
+//     {src: "images/mmm.jpg", alt: "Metro Morning Mix"},
+//     {src: "images/kuul.jpg", alt: "Kull Fm Morning Show"},
+//     {src: "images/pempa.jpg", alt: "Peace Mobile Preachers Association"},
+//     {src: "images/star.jpg", alt: "Starr Luvv"},
+//     {src: "images/tdrive.jpg", alt: "Testimony Drive"},
+// ];
+
 export const images = [
-    {src: "images/ediot.jpg", alt: "DJ Koded Ediot Refix"},
-    {src: "images/lims.jpg", alt: "Lims Radio"},
-    {src: "images/byfball.jpg", alt: "Bekwai Youth Football Academy"},
-    {src: "images/mmm.jpg", alt: "Metro Morning Mix"},
-    {src: "images/kuul.jpg", alt: "Kull Fm Morning Show"},
-    {src: "images/pempa.jpg", alt: "Peace Mobile Preachers Association"},
-    {src: "images/star.jpg", alt: "Starr Luvv"},
-    {src: "images/tdrive.jpg", alt: "Testimony Drive"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367442/works/ediot_j0b3si.jpg", alt: "DJ Koded Ediot Refix"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367461/works/lims_vvwdx7.jpg", alt: "Lims Radio"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367441/works/byfball_oqzkj5.jpg", alt: "Bekwai Youth Football Academy"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367468/works/mmm_kvv94q.jpg", alt: "Metro Morning Mix"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367460/works/kuul_pi2ish.jpg", alt: "Kull Fm Morning Show"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367455/works/pempa_jrsveq.jpg", alt: "Peace Mobile Preachers Association"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367437/works/star_zwvhko.jpg", alt: "Starr Luvv"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367440/works/tdrive_cbyxy7.jpg", alt: "Testimony Drive"},
 ];
 
 export const videos = [
@@ -16,7 +27,12 @@ export const videos = [
     {link: "https://www.youtube.com/watch?v=GE8i_JedvKM", alt: "Monalisa Dance Video by Team Anieden Academy"},
 ];
 
+// export const webs = [
+//     {src: "images/cuultunez.jpg", alt: "CuulTunezGh", link: "https://cuultunezgh.wordpress.com/"},
+//     {src: "images/jobcentergh.jpg", alt: "Jobcentregh", link: "http://jobcentregh.com/"},
+// ];
+
 export const webs = [
-    {src: "images/cuultunez.jpg", alt: "CuulTunezGh", link: "https://cuultunezgh.wordpress.com/"},
-    {src: "images/jobcentergh.jpg", alt: "Jobcentregh", link: "http://jobcentregh.com/"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367455/works/cuultunez_keavdu.jpg", alt: "CuulTunezGh", link: "https://cuultunezgh.wordpress.com/"},
+    {src: "https://res.cloudinary.com/netkid/image/upload/v1609367439/works/jobcentergh_v6zqqo.jpg", alt: "Jobcentregh", link: "http://jobcentregh.com/"},
 ];
